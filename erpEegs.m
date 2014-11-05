6bb736cc0dd92d0cb2b1f22b345fd05ebccab5a5
lbuser <joftheancientgermanspear@users.noreply.github.com>
2014-11-05 16:56:47 -0500

    more intuitive name & example

--- erpEegs.m
+++ erpEegs.m
@@ -1,7 +1,15 @@
-function pipelineInputs = erpEegs(tests, mffPathBase)
-%function pipelineInputs = erpEegs(tests, mffPathBase)
+function pipelineInputs = erpEegs(tests, mffBasePath)
+%function pipelineInputs = erpEegs(tests, mffBasePath)
+%for example:
+%   tests = {'hand1', 'hand2', 'hand3', 'lang1', 'lang2'};
+%   mffBasePath = '/depot/home/lbuser/data/eeg1028/mffs/';
+%assumptions:
+%   mff import installed, see help prepEvents
+%   spm installed
+%   mff files can be found by concatenating mffBasePath with test name
+%       /depot/home/lbuser/data/eeg1028/mffs/lang2.mff
 
-pipelineInputs = cellfun(@(t) createPipelineInput(t, mffPathBase), tests, 'UniformOutput', false);
+pipelineInputs = cellfun(@(t) createPipelineInput(t, mffBasePath), tests, 'UniformOutput', false);
 pipelineInputs = [pipelineInputs{:}];
 
 end
